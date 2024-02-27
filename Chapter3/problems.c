e4821c481ec0c22a5100421d1915a07a1ca49bb0
Christian Adams <ChristianAdams7089@gmail.com>
2024-02-27 14:11:54 -0500

    Finished problem number 1, made number guesser game

--- Chapter3/problems.c
+++ Chapter3/problems.c
@@ -1,36 +1,46 @@
 // The Chapter 3 end of chapter problems
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
 //A number guessing game, using the isdigit() function, random number generation, and user input.
 
     int randomNumber()
     {
+        srand(time(NULL));
+        int r = (rand() % 10);
 
-
+        return r;
     }
 
-    int enterADigit(char input){
 
-        char cUserInput = input;
+    int problemOne(){
 
-        if (isdigit(cUserInput) == 1){
-            printf("Good job, that is a digit.\n");
-            return 1;
+        int rNum = randomNumber();
+        char userInput = ' ';
+
+        printf("Guess a number from 0 to 9: ");
+        scanf("%c", &userInput);
+
+        if (isdigit(userInput) >= 1) {
+            printf("The Random Number was %d, and you guessed %c\n", rNum, userInput);
+            if(userInput == rNum){
+                printf("You guessed right.");
+            }
+            else {
+                printf("You guessed wrong.");
+            }
         }
         else {
             printf("That is not a digit.\n");
-            return 0;
         }
+
     }
+
 
 int main()
 {
-    printf("Enter a digit: ");
-    int user;
-
-    
-    int digitCheck = enterADigit();
+    problemOne();
 
     return 0;
 }
